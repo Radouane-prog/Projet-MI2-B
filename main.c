8b867fc6c785676e2a19fd33cfd3eef6464dbf80
Radouane-prog <hadj.radouane1@gmail.com>
2025-04-14 03:39:39 +0200

    Add files via upload
    Nous avons rendu le début du programme main.c avec uniquement la structure pour le moment. Nous avancerons le projet plus profondément durant les vacances.

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+//Structure permettant de gérer un Animal
+typedef struct{
+        int id;
+        char nom[25];
+        char espece[25];
+        int annee;
+        float poids;
+        char citation;
+    }animal;
+
+int main(){
+    
+    
+    return 0;
+}
