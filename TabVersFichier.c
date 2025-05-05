a3bdc05746a99f752cf6c106f0f32324e6499916
Rehanensr <rehanenisar95@gmail.com>
2025-05-05 16:56:44 +0200

    Update TabVersFichier.c

--- TabVersFichier.c
+++ TabVersFichier.c
@@ -1,12 +1,14 @@
 void TabVersFichier(Animal tab[], int *taille){
     FILE *fichier;
-    fichier = fopen("ChenYI_Tech/animaux.txt","w");
+    fichier = fopen("animaux.txt","w");
     if(fichier == NULL){
         printf("Fichier introuvable. \n");
         return;
     }
-    for(int i=0;i < *taille-1;i++){
-        fprintf("%d %s %s %d %fKg %s\n",tab[i].id, tab[i].nom, tab[i].espece, tab[i].annee, tab[i].poids, tab[i].citation);
+    for(int i=0;i < *taille;i++){
+        if( (tab[i].id != -5) || (tab[i].nom != "NULL") || (tab[i].espece != "NULL") || (tab[i].annee != -5) || (tab[i].poids != -5.0) || (tab[i].citation != "NULL")){
+            fprintf(fichier, "%d %s %s %d %f %s\n",tab[i].id, tab[i].nom, tab[i].espece, tab[i].annee, tab[i].poids, tab[i].citation);   
+        }
     }
     
     fclose(fichier);
