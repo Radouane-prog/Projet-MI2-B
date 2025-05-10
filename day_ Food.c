c8e39fe419970f01625e006ce266ece5776d27db
Radouane-prog <hadj.radouane1@gmail.com>
2025-05-10 18:55:46 +0200

    Update day_ Food.c
    corrections

--- day_ Food.c
+++ day_ Food.c
@@ -3,7 +3,7 @@ float day_Food(Animal tab[], int taille) {
     float consommation = 0;
 
     for(int i=0;i<taille;i++){
-        if (str_Egale(tab[i].espece, "chat") || str_Egale(tab[i].espece, "chien")) {
+        if (strEgale(tab[i].espece, "chat") || strEgale(tab[i].espece, "chien")) {
         if (tab[i].age < 2) {
             consommation += 500;
         } else {
@@ -11,16 +11,16 @@ float day_Food(Animal tab[], int taille) {
         }
 
         // Affichage spécifique selon l'espèce
-        if (str_Egale(tab[i].espece, "chat")) {
+        if (strEgale(tab[i].espece, "chat")) {
         } else {
         }
     } 
     // Vérification pour le Hamster
-    else if (str_Egale(tab[i].espece, "hamster")) {
+    else if (strEgale(tab[i].espece, "hamster")) {
         consommation += 20;
     } 
     // Vérification pour l'Autruche
-    else if (str_Egale(tab[i].espece, "autruche")) {
+    else if (strEgale(tab[i].espece, "autruche")) {
         consommation += 2500;
     }
     consommation /= 1000;
