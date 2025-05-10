85bf03f069769e710daf39239a7adb959c30b244
talal95c <talalaamir958@gmail.com>
2025-05-10 21:08:49 +0200

    Update Fichier_ vers_Tab.c
    modification la  ca marche nrmlment

--- Fichier_ vers_Tab.c
+++ Fichier_ vers_Tab.c
@@ -1,5 +1,6 @@
 
-void retirer_saut_de_ligne(char *chaine){
+void retirer_saut_de_ligne(char *chaine) {
+    int i = 0;
     while (chaine[i] != '\0') {
         if (chaine[i] == '\n') {
             chaine[i] = '\0';
@@ -13,15 +14,15 @@ void retirer_saut_de_ligne(char *chaine){
 int lire_animaux_fichier(char *nomFichier, Animal tab[], int capacite) {
     FILE *f = fopen(nomFichier, "r");
     
-    if (f==NULL) {
-        printf("Impossible d'ouvrir le fichier \n");
+    if (f == NULL) {
+        printf("Impossible d'ouvrir le fichier.\n");
         return -1;
     }
 
-    char ligne[50];
-    int  compteur = 0;
+    char ligne[256]; // taille augmentée pour stocker toute une ligne
+    int compteur = 0;
 
-    while (fgets(ligne,50, f) && compteur < capacite) {
+    while (fgets(ligne, sizeof(ligne), f) && compteur < capacite) {
         retirer_saut_de_ligne(ligne);
 
         Animal a;
@@ -38,6 +39,8 @@ int lire_animaux_fichier(char *nomFichier, Animal tab[], int capacite) {
 
         if (nb == 6) {
             tab[compteur++] = a;
+        } else {
+            printf("Ligne mal formatée ignorée : %s\n", ligne);
         }
     }
 
