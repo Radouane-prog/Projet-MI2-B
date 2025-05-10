e2a81d2c87590b8249224c318169fcfeb1e57587
Radouane-prog <hadj.radouane1@gmail.com>
2025-05-10 18:49:32 +0200

    Update fonctions_strings.c
    Ajout de la fonction strEgale pour les comparaisons de chaînes de caractères

--- fonctions_strings.c
+++ fonctions_strings.c
@@ -7,3 +7,13 @@ void copier_chaine(char *dest, const char *src) {
     }
     *dest = '\0';
 }
+
+//fonction permettant de comparer deux chaînes de caractères
+int strEgale(char *a, char *b) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        if (a[i] != b[i]) return 0;
+        i++;
+    }
+    return (a[i] == '\0' && b[i] == '\0');
+}
