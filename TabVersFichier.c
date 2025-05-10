2ea5ccc12264620307b3579457715d199dcf15ca
Radouane-prog <hadj.radouane1@gmail.com>
2025-05-10 19:44:25 +0200

    Update TabVersFichier.c
    corrections

--- TabVersFichier.c
+++ TabVersFichier.c
@@ -1,22 +1,3 @@
-//fonction permettant d'assigner une chaîne de caractère à un tableau de caractères
-void copier_chaine(char *dest, const char *src) {
-    while (*src) {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-}
-
-//fonction permettant de comparer deux chaînes de caractères
-int strEgale(char *a, char *b) {
-    int i = 0;
-    while (a[i] != '\0' && b[i] != '\0') {
-        if (a[i] != b[i]) return 0;
-        i++;
-    }
-    return (a[i] == '\0' && b[i] == '\0');
-}
 
 //fonction permettant de stocker dans le fichier animaux.txt les données du tableaux
 void TabVersFichier(Animal tab[], int *taille) {
